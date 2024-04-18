ef0cf7f7255f27ff86222a0537fdb3f4152656a0
masianH <masian.huneau@gmail.com>
2024-04-18 18:21:29 +0200

    cnf vers graphe sans lecture

--- cnfToGraph.py
+++ cnfToGraph.py
@@ -1,10 +1,10 @@
 ##### packages
 import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
 
 plt.figure(figsize=(8, 8))
-#### Fonctions
+
+#### Functions
 
 def get_base_graph(nb_var: int):  # -> nx.Graph
     G = nx.Graph()
@@ -12,50 +12,66 @@ def get_base_graph(nb_var: int):  # -> nx.Graph
     for i in range(1, nb_var+1):
         G.add_edges_from([(str(i), str(-i)), (str(i), 'N'), (str(-i), 'N')])
     return G
-    
 
-def add_or2(G, n_cl: int, en1: str, en2: str) -> str:
-    L = 'L'+n1+str(n_cl)
-    R = 'R'+n2+str(n_cl)
-    out = n1+n2+str(n_cl) 
-    G.add_edges_from([(en1, L), (en2, R), (L, R), (out, L), (out, R)])
-    return out
+def add_clause(G, clause_nb: int, x1: str, x2: str, x3: str) -> None:
+    N_x1 = f"N_{x1}_{clause_nb}"; N_x2 = f"N_{x2}_{clause_nb}"
+    N_x3 = f"N_{x2}_{clause_nb}"; dij_x1x2 = f"dij_{x1}{x2}_{clause_nb}"
+    N_x1x2 = f"N_{x1}{x2}_{clause_nb}"; dij_x1x2x3 = f"dij_{x1}{x2}{x3}_{clause_nb}"
+    G.add_edges_from([(x1, N_x1), (x2, N_x2), (N_x1, N_x2), (N_x1, dij_x1x2), (N_x2, dij_x1x2),
+                      (dij_x1x2, N_x1x2), (x3, N_x3), (N_x1x2, N_x3), (N_x1x2, dij_x1x2x3),
+                      (N_x3, dij_x1x2x3), (dij_x1x2x3, 'F'), (dij_x1x2x3, 'N')])
 
-def add_or(G, en1: str, en2: str, en3: str) -> str:
-    
-    out = add_or2(G, add_or2(G, en1, en2), en3)
-    G.add_edges_from([(out, 'F'), (out, 'N')])
-    
+def get_graph_from_cnf(list_cnf: list, nb_vars: int): # -> nx.Graph
+    G = get_base_graph(nb_vars)
+    for clause_nb in range(len(list_cnf)):
+        add_clause(G, clause_nb+1, list_cnf[clause_nb][0], 
+                   list_cnf[clause_nb][1], list_cnf[clause_nb][2])
+    return G
 
-#### Colors
+#### Coloring
 
-def get_dict_link(dict_int: dict) -> dict:
+def get_dict_links(dict_int: dict) -> dict:
     d = {}
     int_F = dict_int['F']; int_V = dict_int['V']; int_N = dict_int['N']
     d[int_F] = 'red'; d[int_V] = 'green'; d[int_N] = 'blue'
-    for i in range(4):
-        if i not in [int_F, int_V, int_N]:
-            d[i] = 'grey'
     return d
     
-def get_list_colors(dict_int: dict, dict_link: dict) -> list:
-    return [dict_link[dict_int[name]] for name in G.nodes()]
+def get_dict_colors(dict_colors_int: dict, dict_links: dict) -> dict:
+    return {node: dict_links[dict_colors_int[node]] for node in dict_colors_int.keys()}
+
+def get_list_colors(dict_colors: dict) -> list:
+    return [dict_colors[node] for node in G.nodes()]
+
+def is_a_var(x: str) -> bool:
+    return x[0] not in ['V', 'F', 'N', 'd', '-']
+
+def get_dict_bin_vars(dict_colors: dict) -> dict:
+    return {node: dict_colors[node] == 'green' for node in dict_colors.keys() if is_a_var(node)}
+
+def graph_3_coloring(G, nb_vars: int, with_fig=True) -> list:
+    dict_coloring_int = nx.greedy_color(G, 'DSATUR')
+    if 3 in dict_coloring_int.values():
+        return [False, {}]
+    dict_links = get_dict_links(dict_coloring_int)
+    dict_colors = get_dict_colors(dict_coloring_int, dict_links)
+    res_list = [True]
+    res_list.append(get_dict_bin_vars(dict_colors))
+    if not with_fig:
+        return res_list
+    list_colors = get_list_colors(dict_colors)
+    nx.draw_networkx(G, node_color = list_colors)   # Draw graph
+    return res_list
 
 
-#### Tests
+#### Main Program
 
-nb_var = 3
-G = get_base_graph(nb_var)
-add_or3(G, '1', '2', '3')
-add_or3(G, '1', '2', '-3')
-d = nx.greedy_color(G, 'DSATUR')
+list_cnf_formula = [('1', '2', '3'),
+                    ('1', '-2', '-3')]
+nb_vars = 3
 
-#print(d)
+G = get_graph_from_cnf(list_cnf_formula, nb_vars)
+solv_list = graph_3_coloring(G, nb_vars)
+print(solv_list)
 
-# Draw graph
-nx.draw_networkx(G,
-                node_color = get_list_colors(d, get_dict_link(d)), 
-                with_labels=True,
-                font_color = 'black')
-#ShowFigure
-plt.show()
+if solv_list[0]:
+    plt.show()                                      # ShowFigure
