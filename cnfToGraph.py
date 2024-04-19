27dbbd02521e00101b217ed9773153862f7f8267
Huneau Masian <mashu@fixe-g204-19>
2024-04-19 16:58:46 +0200

    lecture cnf and pos for small graph

--- cnfToGraph.py
+++ cnfToGraph.py
@@ -1,14 +1,17 @@
 ##### import modules
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
+from pysat.formula import CNF
 
-plt.figure(figsize=(8, 8))
+current_path = '/home/mashu/Documents/concretisation/'
+plt.figure(figsize = (15, 15))
 
 #### Functions
 
 ### Create graph
 
-def get_graph_base(nb_var):
+def get_graph_base(pos: dict, nb_var: int):
     """ returns a graph that will be used to implement the clauses
 
     Parameters
@@ -24,113 +27,118 @@ def get_graph_base(nb_var):
     """
     G = nx.Graph()
     G.add_edges_from([('T', 'F'), ('N', 'F'), ('T', 'N')])
+    pos['T'] = np.array([0, 5])
+    pos['F'] = np.array([2, 5])
+    pos['N'] = np.array([1, 3])
     for i in range(1, nb_var+1):
         G.add_edges_from([(str(i), str(-i)), (str(i), 'N'), (str(-i), 'N')])
+        pos[str(i)] = np.array([i-1, 2]); pos[str(-i)] = np.array([i-0.5, 2])
     return G
-
-# Retrieving a list of values from the CNF file
-def read(fic): # Sullivan
-    with open(fic,"r") as f:
-        lines = f.readlines()  
-    val=[]
-    for line in lines:
-        if not line.startswith('c') and not line.startswith('p') and not line.startswith('%'):
-            nums = (int(x) for x in line.split() if x!= "0")
-            val.extend(nums)
-    return val
-
-# CNF list in 3-uplets
-def arrangement_3(fic): # Sullivan
-    list_decomp = []
-    val = read(fic)
-    for i in range(0,len(read(fic)),3):
-        list_decomp.append([val[i],val[i+1],val[i+2]])
-    return list_decomp
-
-def list_clauses(fic): #Sullivan
-    read(fic)
-    return arrangement_3(fic)
     
-def add_clause(G, clause_nb, x1, x2, x3) :
-    """ adds a disjunction clause with input variables x1, x2 and x3 to the graph 
+def add_clause(G, pos: dict, clause_nb: int, x1: int, x2: int, x3: int) :
+    """ adds a disjunction clause with input variables x1, x2, x3 to the graph 
 
     Parameters
     -----------
     G: Networkx graph
         graph use to implement clauses
     clause_nb: int
-        clause's number in the cnf formula
-    x1, x2, x3 : str
+        clause number in the cnf formula
+    x1, x2, x3 : int
         clause input variable names
     """
-    # set names
-    N_x1 = f"N_{x1}_{clause_nb}"; N_x2 = f"N_{x2}_{clause_nb}"
-    N_x3 = f"N_{x2}_{clause_nb}"; dij_x1x2 = f"dij_{x1}{x2}_{clause_nb}"
-    N_x1x2 = f"N_{x1}{x2}_{clause_nb}"; dij_x1x2x3 = f"dij_{x1}{x2}{x3}_{clause_nb}"
+    # set names and positions
+    N_x1 = f"N_{x1}_{clause_nb}"; 
+    pos[N_x1] = np.array([(clause_nb-1)*2, 0])
+    N_x2 = f"N_{x2}_{clause_nb}"; 
+    pos[N_x2] = np.array([(clause_nb-1)*2+1, 0])
+    dij_x1x2 = f"dij_{x1}{x2}_{clause_nb}"; 
+    pos[dij_x1x2] = np.array([(clause_nb-1)*2+0.5, -2])
+    N_x1x2 = f"N_{x1}{x2}_{clause_nb}"; 
+    pos[N_x1x2] = np.array([(clause_nb-1)*2+0.5, -3])
+    N_x3 = f"N_{x3}_{clause_nb}"; 
+    pos[N_x3] = np.array([(clause_nb-1)*2+1.5, -3])
+    dij_x1x2x3 = f"dij_{x1}{x2}{x3}_{clause_nb}"; 
+    pos[dij_x1x2x3] = np.array([(clause_nb-1)*2+1, -5])
     # add edges (and nodes)
-    G.add_edges_from([(x1, N_x1), (x2, N_x2), (N_x1, N_x2), (N_x1, dij_x1x2), (N_x2, dij_x1x2),
-                      (dij_x1x2, N_x1x2), (x3, N_x3), (N_x1x2, N_x3), (N_x1x2, dij_x1x2x3),
-                      (N_x3, dij_x1x2x3), (dij_x1x2x3, 'F'), (dij_x1x2x3, 'N')])
+    G.add_edges_from([(str(x1), N_x1), (str(x2), N_x2), (N_x1, N_x2), 
+                      (N_x1, dij_x1x2), (N_x2, dij_x1x2), (dij_x1x2, N_x1x2), 
+                      (str(x3), N_x3), (N_x1x2, N_x3), (N_x1x2, dij_x1x2x3),
+                      (N_x3, dij_x1x2x3), (dij_x1x2x3, 'F'), 
+                      (dij_x1x2x3, 'N')])
 
-def get_graph_from_cnf(list_cnf, nb_vars): 
+def get_graph_from_cnf(cnf_formula): 
     """ returns the graph corresponding to the cnf formula
 
     Parameters
     -----------
-    list_cnf: list
-        list of 3-uplet of literals (str), each corresonding to a clause
-    nb_vars: int
-        number of clauses in the formula
+    cnf_formula: pysat.formula CNF()
+        list of 3-uplet of literals (int), each corresonding to a clause
     
     Returns
     --------
     G: Networkx graph
         graph that contains variables and clauses
     """
-    G = get_graph_base(nb_vars)
-    for clause_nb in range(len(list_cnf)):
-        add_clause(G, clause_nb+1, list_cnf[clause_nb][0], 
-                   list_cnf[clause_nb][1], list_cnf[clause_nb][2])
-    return G
+    pos = {}
+    G = get_graph_base(pos, cnf_formula.nv)
+    clauses = cnf_formula.clauses
+    for clause_nb in range(len(clauses)):
+        add_clause(G, pos, clause_nb+1, clauses[clause_nb][0], 
+                   clauses[clause_nb][1], clauses[clause_nb][2])
+    return G, pos
 
 ### Coloring
 
-def get_dict_links(dict_int):
-    """ returns a dict that links int and colors according to our desired color references
+def get_dict_links(dict_int: dict) -> dict:
+    """ returns a dict that links int and colors according to our desired 
+    color references
 
     Parameters
     -----------
     dict_int: dict
-        dict with graph nodes (str) as keys and integers representing colors as values 
+        dict with graph nodes (str) as keys and integers representing colors as
+        values 
     
     Returns
     --------
     dict_links: dict
-        dict whose keys are integers and whose values are the corresponding colors (str)
+        dict whose keys are integers and whose values are the corresponding 
+        colors (str)
     """
     dict_links = {}
-    int_F = dict_int['F']; int_V = dict_int['T']; int_N = dict_int['N']
-    dict_links[int_F] = 'red'; dict_links[int_V] = 'green'; dict_links[int_N] = 'blue'
+    int_F = dict_int['F']; int_T = dict_int['T']; int_N = dict_int['N']
+    dict_links[int_F] = 'red'; dict_links[int_T] = 'green' 
+    dict_links[int_N] = 'blue'
+    for i in range(4):
+        if i not in [int_F, int_T, int_N]:
+            dict_links[i] = 'grey'
     return dict_links
     
-def get_dict_colors(dict_colors_int, dict_links):
-    """ returns a dict that associates graph nodes and colors that will be used for draw
+
+def get_dict_colors(dict_colors_int: dict, dict_links: dict) -> dict:
+    """ returns a dict that associates graph nodes and colors that will be 
+    used for draw
 
     Parameters
     -----------
     dict_colors_int: dict
-        dict with graph nodes (str) as keys and integers representing colors as values 
+        dict with graph nodes (str) as keys and integers representing colors 
+        as values 
     dict_links: dict
-        dict whose keys are integers and whose values are the corresponding colors (str)
+        dict whose keys are integers and whose values are the corresponding 
+        colors (str)
 
     Returns
     --------
     unamed: dict
         dict with graph nodes (str) as keys and colors as values (str)
     """
-    return {node: dict_links[dict_colors_int[node]] for node in dict_colors_int.keys()}
+    return {node: dict_links[dict_colors_int[node]] for node in 
+            dict_colors_int.keys()}
 
-def get_list_colors(G, dict_colors):
+
+def get_list_colors(G, dict_colors: dict) -> list:
     """ returns a list of colors in graph nodes order
 
     Parameters
@@ -147,6 +155,7 @@ def get_list_colors(G, dict_colors):
     """
     return [dict_colors[node] for node in G.nodes()]
 
+
 def is_a_var(name: str) -> bool:
     """ returns the answer to: is the node a variable?
 
@@ -160,64 +169,69 @@ def is_a_var(name: str) -> bool:
     unamed: bool
         True if node is a variable else False
     """
-    return name[0] not in ['V', 'F', 'N', 'd', '-'] # non-variable nodes begin with these characters
+    return name[0] not in ['T', 'F', 'N', 'd', '-'] # non-variable nodes begin with these characters
 
-def get_dict_bin_vars(dict_colors):
-    """ returns a dict that associate variable nodes and boolean values according to their color
+
+def get_dict_bin_vars(dict_colors: dict):
+    """ returns a dict that associate variable nodes and boolean values 
+    according to their color
 
     Parameters
     -----------
     dict_colors: dict
         dict that associates graph nodes and colors that will be used for draw
+        
     Returns
     --------
     unamed: dict
         dict with variable nodes (str) as keys and booleans as values
     """
-    return {node: dict_colors[node] == 'green' for node in dict_colors.keys() if is_a_var(node)}
+    return {node: dict_colors[node] == 'green' for node in dict_colors.keys() 
+            if is_a_var(node)}
 
-def graph_3_coloring(G, nb_vars: int, with_draw=True) -> list:
-    """ returns a list of solutions deduced from the coloring and draws the graph 
-    if possible and desired
+
+def graph_3_coloring(G, nb_clauses: int) -> list:
+    """ returns a list of solutions deduced from the coloring and draws the 
+    graph if possible
 
     Parameters
     -----------
     G: Networkx graph
         graph on which we have implemented the cnf formula
-    nb_vars: int
-        number of variables in the cnf formula
-    with_draw: bool
-        optional, default value is True, must be set False if drawing is not desired
+    nb_clauses: int
+        number of clauses in cnf formula implemented    
     
     Returns
     --------
-    res_list: list
-        list having as first element a boolean corresponding to the success of 3-coloring, 
-        as second a dictation associating variables (str) and their values (bool) 
-        (empty if no 3-coloring), and as third a boolean corresponding to drawing execution
+    unamed: list
+        list having as first element res_list (list) descirbe by
+            list having as first element a boolean corresponding to the success 
+            of 3-coloring, as second a dictation associating variables (str) 
+            and their values (bool) (empty if no 3-coloring), and as third a 
+            boolean corresponding to drawing execution
+        and dict_coloring_int (dict) as second element
     """
     res_list = [False, {}, False]
     dict_coloring_int = nx.greedy_color(G, 'DSATUR')
-    if 3 not in dict_coloring_int.values():
-        dict_links = get_dict_links(dict_coloring_int)
-        dict_colors = get_dict_colors(dict_coloring_int, dict_links)
-        res_list[0] = True
-        res_list[1] = get_dict_bin_vars(dict_colors)
-        if with_draw:  
-            res_list[2] = True
-            list_colors = get_list_colors(G, dict_colors)
-            nx.draw_networkx(G, node_color = list_colors)   # Draw graph
-    return res_list
-
+    #if 3 not in dict_coloring_int.values():
+    dict_links = get_dict_links(dict_coloring_int)
+    dict_colors = get_dict_colors(dict_coloring_int, dict_links)
+    res_list[0] = True
+    res_list[1] = get_dict_bin_vars(dict_colors)
+    if nb_clauses <= 3:  
+        res_list[2] = True
+        list_colors = get_list_colors(G, dict_colors)
+        nx.draw_networkx(G, node_color = list_colors, pos = pos)   # Draw graph
+    return res_list, dict_coloring_int
 
 #### Main Program
 
-list_cnf_formula = list_clauses("clause.cnf") # Sullivan
-nb_vars = len(list_cnf_formula)*3 # Sullivan
+cnf_formula = CNF(from_file=current_path+'simple_formula.cnf')
 
-G = get_graph_from_cnf(list_cnf_formula, nb_vars)
-solv_list = graph_3_coloring(G, nb_vars)
-print(solv_list[:2])
+G, pos = get_graph_from_cnf(cnf_formula)
+solv_list = graph_3_coloring(G, len(cnf_formula.clauses))
+print("dict_coloring :", solv_list[1])
+print("solv by graph :", solv_list[0][:2])
 
-if solv_list[2]:
+if solv_list[0][2]:
     plt.show()                                      # ShowFigure
