5e371ec1d0b66f782f12bcb7b0b4c6fa14ab6728
masianH <masian.huneau@gmail.com>
2024-05-06 22:59:50 +0200

    first struct

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 #### imports
 
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+from pysat.formula import CNF
+from pysat.solvers import Solver
+
+wm = plt.get_current_fig_manager()      # > plt fullscreen
+wm.window.state('zoomed')               #/
 
 
 #### solving cnf, building and coloring the corresponding graph
 
+def main_1():
+    """
 
+    """
 
 #### graph coloring with solver sat
 
+def main_2():
+    """
 
+    """
 
 #### main program
+
+def main():
+    """
+    
+    """
+
+main()
