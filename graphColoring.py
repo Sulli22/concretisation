25e142af99a16e447f712d6eca3c5c57f7c111f9
masianH <masian.huneau@gmail.com>
2024-05-06 11:10:00 +0200

    add input for graph nodes and edges

--- graphColoring.py
+++ graphColoring.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 from pysat.formula import CNF
 from pysat.solvers import Solver
 
-wm = plt.get_current_fig_manager()      # > plt fullscreen
-wm.window.state('zoomed')               #/
-
 ####
 
 def get_cnf_from_graph(G):
@@ -30,30 +27,40 @@ def get_cnf_from_graph(G):
 
     return cnf
 
-def get_list_color(cnf):
-    with Solver(bootstrap_with=cnf) as solver:
+def get_list_color(cnf, G):
+    """
+    
+    """
+    with Solver(bootstrap_with = cnf) as solver:
         if not solver.solve():
             return False
-        
         model = [n for n in solver.get_model() if n > 0]
+
     dict_colors = {}
-    dict_links = {1: 'green', 2: 'red', 3: 'blue'}
     for n in model:
         n = str(n)
-        dict_colors[int(n[-1])] = dict_links[int(n[0])]
+        dict_colors[int(n[-1])] = {'1': 'green', '2': 'red', '3': 'blue'}[n[0]]
     return [dict_colors[n] for n in G.nodes]
 
-####
+#### main
+nb_nodes = int(input("number of nodes : "))
+nb_edges = int(input("number of edges : "))
 
-G = nx.Graph()
-
-G.add_edges_from([(1, 2), (2, 3), (1, 3), (1, 4), (2, 4)])
-
+G = nx.gnm_random_graph(nb_nodes, nb_edges)
 cnf_formula = get_cnf_from_graph(G)
+pos = nx.spring_layout(G)
 
+plt.subplot(121)
+plt.title("uncolored graph")
+nx.draw_networkx(G, pos = pos)   # Draw graph
 
-list_colors = get_list_color(cnf_formula)
+list_colors = get_list_color(cnf_formula, G)
 if list_colors: 
-    nx.draw_networkx(G, node_color = list_colors)   # Draw graph
+    plt.subplot(122)
+    plt.title("colored graph")
+    nx.draw_networkx(G, pos = pos, node_color = list_colors)   # Draw graph
+
+wm = plt.get_current_fig_manager()      # > plt fullscreen
+wm.window.state('zoomed')               #/
 
 plt.show()
