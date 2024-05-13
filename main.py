90b421e3f5fd3ce9f8ad539dd7f4abfa1ed68ce9
masianH <masian.huneau@gmail.com>
2024-05-13 15:41:28 +0200

    start main and ask file

--- main.py
+++ main.py
@@ -6,12 +6,42 @@ import numpy as np
 from pysat.formula import CNF
 from pysat.solvers import Solver
 
-#### solving cnf, building and coloring the corresponding graph
+#### cnf to graph
 
-def main_cnf2graph():
+def get_cnf_from_file(file: str):
+    """
+    
+    """
+    try:
+        return CNF(from_file = file + ".cnf")
+    except:
+        file = input("file don't find, file name (without .cnf): ")
+        get_cnf_from_file(file + ".cnf")
+
+def main_cnf2graph_DASTUR():
     """
 
     """
+    formula_file = input("file name (without .cnf): ")
+    cnf_formula = get_cnf_from_file(formula_file) 
+
+#### cnf to graph pysat solver
+
+def main_cnf2graph_SOLVER():
+    """
+    
+    """
+    formula_file = input("file name (without .cnf): ")
+    cnf_formula = get_cnf_from_file(formula_file)
+
+#### cnf to graph pyscp
+
+def main_cnf2graph_PYCSP():
+    """
+    
+    """
+    formula_file = input("file name (without .cnf): ")
+    cnf_formula = get_cnf_from_file(formula_file)
 
 #### graph coloring with solver sat
 
@@ -21,6 +51,24 @@ def main_graph_coloring_with_solver():
     """
 
 #### main program
+
+
+run = True
+
+while run:
+    print("1 - cnf to graph dsatur")
+    print("2 - cnf to graph to cnf")
+    print("3 - cnf to graph pycsp")
+    print("4 - graph to cnf")
+    print("5 - quit")
+    list_choices = ['1', '2', '3', '4', '5']
+    rep = input("choice : ")
+    while rep not in list_choices:
+        rep = input("wrong, try again : ")
+    if rep == '5':
+        run = False
+    
+
 """
 wm = plt.get_current_fig_manager()      # > plt fullscreen
 wm.window.state('zoomed')               #/
